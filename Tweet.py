c7ae451b3d2da640e28139afb6a0bbe8b57a48d7
Leah-Liu <leahliu61@gmail.com>
2017-10-15 18:58:29 -0400

    Delete Tweet.py

--- Tweet.py
+++
@@ -1,9 +0,0 @@
-
-class Tweet:
-    """
-    Base class for Tweet instance
-    """
-    def __init__(self, userId, message, time):
-        self.userId = userId # - nodeId
-        self.message = str(message)
-        self.time = time # - local time
